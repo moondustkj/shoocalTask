3b928b55288e2f3228470767a879e1be88299be4
komalJituri88 <komaljituri88@gmail.com>
2020-05-29 14:52:06 +0530

    Updates

---
+++ precache-manifest.51adc93194c20ecee991cf6c41a47145.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fd51f619f6e1f649716dc733ac1d8ab",
+    "url": "/shoocalTask/index.html"
+  },
+  {
+    "revision": "c71dea1805510a1cfeba",
+    "url": "/shoocalTask/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "60779f78bb2bf5852b68",
+    "url": "/shoocalTask/static/css/main.f0dadff7.chunk.css"
+  },
+  {
+    "revision": "c71dea1805510a1cfeba",
+    "url": "/shoocalTask/static/js/2.48d2b0f6.chunk.js"
+  },
+  {
+    "revision": "b396b9c7cb89986c0edcd31803c9a394",
+    "url": "/shoocalTask/static/js/2.48d2b0f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60779f78bb2bf5852b68",
+    "url": "/shoocalTask/static/js/main.995776a9.chunk.js"
+  },
+  {
+    "revision": "4ea68dedd0506d3b96ec",
+    "url": "/shoocalTask/static/js/runtime-main.1ed1376f.js"
+  }
+]);
